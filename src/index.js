acfea9623b2728ea5c0ae730d83ffe5b86e2f79c
Lucero Dn <lucero_dn@hotmail.es>
2019-01-30 13:26:59 -0600

    Aggregar funcionalidad de cajas y botones
      - tome valores de caja de mensaje y llave
      - hice funcionar el boton de cifrar
      - puse funcion en cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-const mensaje = ( )  =>  {
-    document.getElementById("mensaje").value;
+const mensaje= () => {
+    document.getElementById(cifrar).value;
 }
+ const handleEncodeClick= () => {
+    const valor= document.getElementById("message").value;
+    const valorllave= document.getElementById("key").value;
+    const result = window.cipher.encode(valor, valorllave);
+    document.getElementById("resultadoEncriptar").value = result;
+    //llave();
+ }
+ 
+const llave = ()  => {
+    const valorllave= document.getElementById("key").value;
+    console.log("numero" + valorllave)
+ }
