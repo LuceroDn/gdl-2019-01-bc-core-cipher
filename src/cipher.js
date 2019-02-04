40ca99837eff5f5576ca74adbfdad0626b0c4e26
Lucero Dn <lucero_dn@hotmail.es>
2019-02-04 17:57:13 -0600

    Se agregó codigo al index.js y se hicieron actualizaciones en html en base al anterior

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,10 @@
-const encode = (message, key) => {
-  return "b";
-
-  let result = "";
-  for (let i = 0; i < message.lenght; i++){
-      let code = message.charCodeAt(i);
-      if (65 <= code && code <=  90)
-      result += String.fromCharCode((code - 65 + key) % 26 + 65).charCodeAt(0);  
-    else if (97 <= code && code <= 122)
-      result += String.fromCharCode((c - 97 + key) % 26 + 97).charCodeAt(0);  
-    else
-      result += message.charAt(i);  
-  }
-  return result;
-}
-
-
 window.cipher = {
-  encode: encode,
-};
+//alphabet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+//encode: (offset, string) => {
+  //const alphabetSize= window.cipher.alphabet.length
+    
+  
+  
+//window.cipher = {
+  //encode: encode,
+ }
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-const mensaje= () => {
-    document.getElementById(cifrar).value;
-}
- const handleEncodeClick= () => {
-    const valor= document.getElementById("message").value;
-    const valorllave= document.getElementById("key").value;
-    const result = window.cipher.encode(valor, valorllave);
-    document.getElementById("resultadoEncriptar").value = result;
-    //llave();
- }
- 
-const llave = ()  => {
-    const valorllave= document.getElementById("key").value;
-    console.log("numero" + valorllave)
- }
+let message;
+let string = [];
+let code = [];
+let n;
+let j;
+let newCode = [];
+
+encode = () => {
+    message = document.getElementById("texto").value;
+    n = document.getElementById("offset").value;
+
+    for (j = 0; j < message,length; j++ ) {
+        string.push((message.charCodeAt(j))-65+n%26+65);
+        code.push(String.fromCharCode(string[j]));
+    }
+    newCode = code.join("");
+    document.getElementById("textResult").innerHTML = (newCode.toUpperCase());
+
+    string = [];
+    code = [];
+};
+
+uncode = () => {
+    message = document.getElementById("texto").value;
+    n = document.getElementById("offset").value;
+
+    for (j = 0; j < message,length; j++ ) {
+        string.push((message.charCodeAt(j))+65-n%26-65);
+        code.push(String.fromCharCode(string[j]));
+    }
+    newCode = code.join("");
+    document.getElementById("textResult").innerHTML = (newCode.toUpperCase());
+
+    string = [];
+    code = [];
+};
+
+
+
+
+
+
+// mensaje= () => {
+  //  document.getElementById(cifrar).value;
+//};
+ //const handleEncodeClick= () => {
+   // const valor= document.getElementById(mensaje - "string").value;
+    //const valorllave= document.getElementById(numero + "offset").value;
+    //const result =window.cipher.encode(valor, valorllave);
+    //document.getElementById("textResult").value = result;
+
+     //}
+  
